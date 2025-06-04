3d403846d85bde4582dbb30acc983537728997c6
Raza-Lab029 <razakimlie200@gmail.com>
2025-06-04 08:38:40 +0800

    Update database.js

--- database.js
+++ database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'razakimlie.1911836',  // your MySQL root password
-  database: 'recipe_box'          // your database name
+  password: 'razakimlie.1911836',  
+  database: 'recipe_box'          
 });
 
 connection.connect((err) => {
